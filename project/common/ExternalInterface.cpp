168a84ba91d2891b24be9bbd9dcc2ef80bff651c
Evgeny <Evgeny>
2021-03-15 17:36:58 +0600

    Revert "update ios sdk to ver 8.0"
    This reverts commit 752a8c4388eac44370effd938970f36810a5a2e4.

--- project/common/ExternalInterface.cpp
+++ project/common/ExternalInterface.cpp
@@ -6,24 +6,39 @@
 #define NEKO_COMPATIBLE
 #endif
 
+
 #include <hx/CFFI.h>
+
 #include <string>
 #include <vector>
+
 #include <AdMobEx.h>
 
 using namespace admobex;
 
 AutoGCRoot* rewardedEventHandle = NULL;
 
-static value admobex_init(value rewarded_id, value on_rewarded_event){
+static value admobex_init(value *args, int count){
+	
+	value on_rewarded_event = args[7];
 	
 	rewardedEventHandle = new AutoGCRoot(on_rewarded_event);
+	
+	bool testing_ads = val_bool(args[4]);
+	bool child_directed_treatment = val_bool(args[5]);
 
-	init(val_string(rewarded_id));
+	std::vector<std::string> rewarded_id_arr;
+	int n = val_array_size(args[2]);
+	for (int i=0;i<n;++i) {
+		std::string str(val_string(val_array_i(args[2], i)));
+		rewarded_id_arr.push_back(str);
+	}
+
+	init("", "", rewarded_id_arr, "", testing_ads, child_directed_treatment);
 
 	return alloc_null();
 }
-DEFINE_PRIM(admobex_init,2);
+DEFINE_PRIM_MULT(admobex_init);
 
 extern "C" void admobex_main () {	
 	val_int(0); // Fix Neko init
@@ -41,10 +56,9 @@ extern "C" int admobex_register_prims () { return 0; }
 
 extern "C" void reportRewardedEvent(const char* event, const char* data)
 {
-	if (rewardedEventHandle == NULL) return;
-
-	if (data == NULL)
+	if(rewardedEventHandle == NULL) return;
+	if(data == NULL)
 		val_call1(rewardedEventHandle->get(), alloc_string(event));
 	else
-		val_call2(rewardedEventHandle->get(), alloc_string(event), alloc_string(data));
+    	val_call2(rewardedEventHandle->get(), alloc_string(event), alloc_string(data));
 }
--- project/include/AdMobEx.h
+++ project/include/AdMobEx.h
@@ -4,9 +4,13 @@
 #ifndef ADMOBEX_H
 #define ADMOBEX_H
 
+
 namespace admobex {
-	void init(const char* rewarded_id);
+
+	void init(const char *BannerID, const char *InterstitialID, std::vector<std::string> &rewardedIDs, const char *gravityMode, bool testingAds, bool tagForChildDirectedTreatment);
+	
 	bool showRewarded(const char* rewarded_id);
+
 }
 
 #endif
